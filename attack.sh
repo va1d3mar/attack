b87180e6667bbc634858807b9c3cf6903edc3268
va1d3mar <Valdemarius666@protonmail.com>
2022-04-13 12:29:27 +0300

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -12,8 +12,6 @@ command=$1
 parameter=$2
 echo "$(date "+%d.%m.%Y %H:%M:%S")" "--== Починаю процедуру атаки=--" >> $logfile
 echo "                    Слава Україні! Героям слава! Смерть путіну!" >> $logfile
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "Надана команда:" "$1" >> $logfile
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "Наданий параметр:" "$2" >> $logfile
 echo "$(date "+%d.%m.%Y %H:%M:%S")" "Виконую переналаштування для автоматизації виконання" >> $logfile
 command=${command#"docker run -it --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest "}
 command=${command%" -t 1000 -p 1200 --rpc 1000 --debug"}
@@ -21,9 +19,7 @@ command="docker run -i --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddo
 echo "$(date "+%d.%m.%Y %H:%M:%S")" "Остаточна команда: " "$command" >> $logfile
 while true 
 do
-tmux select-window -t attack
-tmux select-pane -t 0
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "Починаю роботу докер-контейнеру" >> /var/log/attack.log & tmux send-keys -t 0 C-z "$command" Enter & echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер запустився" >> /var/log/attack.log & sleep 15m && echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер попрацював 15 хвилин, перезапускаю, шоб не висло" >> /var/log/attack.log && systemctl restart docker
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер перезапущено, чекаю 5 секунд до перезапуску циклу, шоб встиг" >> /var/log/resting.log
+echo "$(date "+%d.%m.%Y %H:%M:%S")" "Починаю роботу докер-контейнеру" >> /var/log/attack.log & tmux select-window -t attack & tmux send-keys -t 0 "$command" Enter & echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер запустився" >> /var/log/attack.log & sleep 900 && echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер попрацював 900 тіків, перезапускаю, шоб не висло" >> /var/log/attack.log && tmux send-keys -t 0 C-c Enter
+echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер перезапущено, чекаю 5 тіків до перезапуску циклу, шоб встиг" >> /var/log/resting.log
 sleep 5
 done
