4816798a204b6f3369472f6dcbe7356570dd75f1
va1d3mar <Valdemarius666@protonmail.com>
2022-04-15 10:19:45 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,22 @@
+  GNU nano 6.2                                       install.sh *                                              
 echo "Розпочинаємо процедуру інсталяції"
-if [ whoami != 'root' ];
-then 
-	echo "Треба це робити під root!" && break
-else
-	apt update
-	apt install apt-transport-https ca-certificates curl software-properties-common tmux nano nload git -y
-	curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-	apt update
-	apt-cache policy docker-ce
-	apt install docker-ce
-	clear
-	echo "Інсталяцію пакетів завершено, починаю завантаження скриптів атаки"
-	rm -rf attack
-	git clone -b testing https://github.com/va1d3mar/attack.git
-	cd attack
-	ls
-	echo "^ ^ ^ Ось файли, з якими потрібно працювати ^ ^ ^"
-	echo "readme.txt тут не просто так, бажано ознайомитися"
-fi
+echo ""
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common tmux nano nload git -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update
+apt-cache policy docker-ce
+apt install docker-ce
+echo ""
+echo "Інсталяцію пакетів завершено, починаю завантаження скриптів атаки"
+echo ""
+rm -rf attack
+git clone -b testing https://github.com/va1d3mar/attack.git
+cd attack
+echo ""
+ls
+echo "^ ^ ^ Ось файли, з якими потрібно працювати ^ ^ ^"
+echo "readme.txt тут не просто так, бажано ознайомитися"
+echo ""
+echo "Всьо закінчено, можна атакувати"
