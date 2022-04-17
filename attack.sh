acbcc3e3297670402def20b957cce3ffdc523e35
va1d3mar <Valdemarius666@protonmail.com>
2022-04-17 12:12:19 +0300

    Update attack.sh

--- attack.sh
+++ attack.sh
@@ -54,6 +54,7 @@ function update {
         git clone -b testing https://github.com/va1d3mar/attack.git
         cd attack
         echo 'Оновлення завершено'
+        write_to_log 'Проведено оновлення скриптів з Github'
         kill_environment
 }
 
